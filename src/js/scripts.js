820fba03707bc109b5a24ddb8a16c082c35e9292
Asif <asif@arxoft.com>
2023-07-31 18:08:28 +0500

    Apply min max date on existing datepickers

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -2,6 +2,20 @@
 /* eslint-disable camelcase */
 import '../css/style.scss';
 
+function gwp_maybe_apply_min_max_on_datepicker(datepicker_input) {
+
+	let minDate = jQuery(datepicker_input).data('min-date');
+	let maxDate = jQuery(datepicker_input).data('max-date');
+	
+	if(minDate) {
+		jQuery(datepicker_input).datepicker('option', 'minDate', minDate);
+	}
+
+	if(maxDate) {
+		jQuery(datepicker_input).datepicker('option', 'maxDate', maxDate);
+	}
+}
+
 function gwp_list_datepicker_add_list_item( new_row ) {
 	// run for each existing list row
 	jQuery( new_row ).find( 'input.datepicker' ).each( function() {
@@ -29,16 +43,7 @@ function gwp_list_datepicker_add_list_item( new_row ) {
 	// Set Min date and Max date on the new datepicker field
 	jQuery( new_row ).find( 'input.datepicker' ).each( function() {
 
-		let minDate = jQuery(this).data('min-date');
-		let maxDate = jQuery(this).data('max-date');
-		
-		if(minDate) {
-			jQuery(this).datepicker('option', 'minDate', minDate);
-		}
-
-		if(maxDate) {
-			jQuery(this).datepicker('option', 'maxDate', maxDate);
-		}
+		gwp_maybe_apply_min_max_on_datepicker(this);
 	});
 }
 
@@ -46,6 +51,12 @@ function gwp_list_datepicker_add_list_item( new_row ) {
 jQuery( document ).bind( 'gform_post_render', function( $ ) {
 	// Init date pickers.
 	gformInitDatepicker();
+
+	// Apply min date and max date limit on existing date pickers
+	jQuery.each(jQuery('.datepicker'), function() {
+		gwp_maybe_apply_min_max_on_datepicker(this);
+	});
+
 } );
 
 gform.addAction( 'gform_list_post_item_add', function ( item, container ) {
